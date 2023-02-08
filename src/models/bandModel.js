f90a60408b3e663e666644aedb22f4c3e5c01922
MatheusBenetti <mbbenetti1@gmail.com>
2023-02-08 16:50:12 -0300

    set bandName to unique

--- src/models/bandModel.js
+++ src/models/bandModel.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 const bandSchema = new mongoose.Schema({
   bandName: {
     type: String,
-    required: [true, 'Please enter your band name.']
+    required: [true, 'Please enter your band name.'],
+    unique: true
   },
   genre: {
     type: String
